d6bc08f71606b92b4b7e75217159ce00f437935c
samkhjhb025 <samkhjhb025@student.wethinkcode.co.za>
2025-12-23 11:17:41 +0200

    updated login and register frontend

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/registration_page')
+def registration_page():
+    return render_template('./login_system/register.html')
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8000, debug=True)
