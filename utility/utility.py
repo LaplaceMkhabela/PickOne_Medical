bd5d3ade419949fe27bb32c069904ffa1bd2364e
samkhjhb025 <samkhjhb025@student.wethinkcode.co.za>
2025-12-26 15:18:38 +0200

    defined functions to calculate week,generate graph data for appointment analytics

---
+++ utility/utility.py
@@ -0,0 +1,32 @@
+from datetime import date,timedelta
+
+def get_week(weekday):
+    week = []
+    
+    for n in range(weekday,0,-1):
+        week.append(date.today() - timedelta(days=n))
+        
+    for n in range(7 - weekday):
+        week.append(date.today() + timedelta(days=n))
+        
+    return week
+
+def appointment_graph_data(appointments):
+    graph = {
+        'x': ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'],
+        'y': []
+    }
+    
+    for date in get_week():
+        graph['y'].append(len(appointments[date]))
+        
+    return graph
+        
+def mock_graph():
+    graph = {
+        'x': ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'],
+        'y': [12, 19, 3, 5, 2, 3,9]
+    }
+    
+    return graph
+    
