ab94b25ac55e3a0e760a72a0d1df1ec90d963972
samkhjhb025 <samkhjhb025@student.wethinkcode.co.za>
2025-12-27 20:45:37 +0200

    updated create_session()

--- database/database.py
+++ database/database.py
@@ -52,9 +52,14 @@ class Database:
             return False
         
     def create_session(self,user):
+        accounts = self.read_data()[user['role']]['accounts']
+        
+        for account in accounts:
+            if user['id'] == account['id']:
+                current_user = account
         try:
             with open(os.path.join(os.getcwd(),'database','database_files','sessions','sessions.json'),'w') as file:
-                file.write(json.dumps({"current_user": user}))
+                file.write(json.dumps({"current_user": current_user}))
                 
         except:
             raise ValueError('Failed to create session')
