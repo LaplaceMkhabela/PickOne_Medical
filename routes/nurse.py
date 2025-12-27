07831632cbb53e62b1910c6be52697bcbf4e22d7
samkhjhb025 <samkhjhb025@student.wethinkcode.co.za>
2025-12-27 16:13:05 +0200

    defined patients route

--- routes/nurse.py
+++ routes/nurse.py
@@ -6,3 +6,7 @@ nurse_bp = Blueprint("nurse",__name__)
 @nurse_bp.route("/dashboard",methods=["GET"])
 def dashboard():
     return render_template('./nurse/dashboard.html')
+
+@nurse_bp.route("/patients",methods=["GET"])
+def patients():
+    return render_template('./nurse/patients.html')
