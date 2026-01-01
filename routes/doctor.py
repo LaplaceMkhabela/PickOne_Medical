9d347791ed8d25f90b0113f66673d971ecfbdf3e
samkhjhb025 <samkhjhb025@student.wethinkcode.co.za>
2026-01-01 12:40:50 +0200

    linked AI model

--- routes/doctor.py
+++ routes/doctor.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import render_template,request,jsonify,redirect,url_for
 from utility.nurse_utility import *
+from utility.model import ai_summary
 
 doctor_bp = Blueprint("doctor",__name__)
 
@@ -22,8 +23,13 @@ def reports():
 
 @doctor_bp.route("/record/<id>",methods=["GET","POST"])
 def patient_record(id):
-    record = get_patient_record(str(id))
-    return render_template('./doctor/record.html',record=record)
+    patient = get_patient_record(str(id))
+    date = list(patient['record'].keys())[-1]
+    vitals = patient['record'][date]
+    summary = ai_summary(jsonify(patient['record']))
+    #patient_analytics = get_trends()
+    
+    return render_template('./doctor/record.html',patient=patient,vitals=vitals,summary=summary)
     
 @doctor_bp.route("/view",methods=["GET","POST"])
 def view_patient():
