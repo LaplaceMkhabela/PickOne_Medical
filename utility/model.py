968ecde1718e0f84315a800610c7185774eb0127
Sabelo Mkhabela <transformmkhabela@gmail.com>
2026-01-04 19:19:04 +0200

    defined html_parser()

--- utility/model.py
+++ utility/model.py
@@ -70,3 +70,32 @@ def ai_assistant(medical_history_text):
         })
 
         print(result.content)
+
+
+def html_parser(text):
+    model = ChatGroq(
+        model= "llama-3.3-70b-versatile",
+        temperature=0,
+        api_key= "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"
+    )
+
+
+    template = """
+    You are an expert markdown to html parser.
+    return the expected output only
+    return No record found if the input is empty
+    Here is the text:
+    {text}
+
+    """
+    
+
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+
+
+    result = chain.invoke({
+        "text": text
+    })
+
+    return result.content
