38c353e4fba5f8efbf1aa2a0a98a248c745f5301
Sabelo Mkhabela <transformmkhabela@gmail.com>
2026-01-13 09:21:15 +0200

    fix:decorator syntax error in routes

--- routes/users.py
+++ routes/users.py
@@ -6,15 +6,15 @@ users_bp = Blueprint("users",__name__)
 session = []
 
 
-users_bp.route('/login/page')
+@users_bp.route('/login/page')
 def login_page():
     return render_template('./login_system/login.html')
 
-users_bp.route('/registration/page')
+@users_bp.route('/registration/page')
 def registration_page():
     return render_template('./login_system/register.html')
 
-users_bp.route('/users/login',methods=['POSt','GET'])
+@users_bp.route('/users/login',methods=['POSt','GET'])
 def login():
     user = request.get_json()
     
@@ -34,7 +34,7 @@ def login():
         else:
             return jsonify({'code':'500','msg':'Invalid Login details'})
 
-users_bp.route('/users/register',methods=['POST','GET'])
+@users_bp.route('/users/register',methods=['POST','GET'])
 def register():
     user = request.get_json()
     result = db.register_user(user)
@@ -44,11 +44,11 @@ def register():
     else:
         return jsonify({'code':'500','msg':'Sorry we can\'t create your account right now'})
     
-users_bp.route('/users/welcome')
+@users_bp.route('/users/welcome')
 def welcome_page():
     return render_template('./welcome/welcome.html')
 
-users_bp.route('/users/logout')
+@users_bp.route('/users/logout')
 def logout():
     session.remove(db.current_user()['id'])
     
