16d8ed2f6d13f612bc92523bbea56120152ee411
Sabelo Mkhabela <transformmkhabela@gmail.com>
2026-01-13 09:43:16 +0200

    update:added route for dashboard

--- routes/help_desk.py
+++ routes/help_desk.py
@@ -6,7 +6,43 @@ from utility.nurse_utility import *
 help_desk_bp = Blueprint("help_desk",__name__)
 
 # Help Desk --------------------------------------------------------------------------------------------------------------
-@help_desk_bp.route('appointments',methods=["POST","GET"])
+@help_desk_bp.route('/dashboard',methods=["POST","GET"])
+def dashboard():
+    current_user = db.current_user()
+    
+    if current_user['role'] == 'help_desk':
+        date = datetime.datetime.today().strftime('%Y-%m-%d')
+        appointments = db.get_appointments(current_user)
+        stats = {
+            "patients": 0,
+            "appointments" : 0,
+            "cancelled": 0
+        }
+        
+        try:
+            appointments = appointments[date]
+            
+            for appointment in appointments:
+                if appointment['status'] == 'Confirmed':
+                    stats['appointments'] = stats.get('appointments') + 1
+                    
+                else:
+                    stats['cancelled'] = stats.get('cancelled') + 1
+                
+                stats['patients'] = stats.get('patients') + 1
+            
+        except:
+            appointments = "Nothing yet"
+        
+        
+        return render_template('./help_desk/dashboard.html',user=current_user,appointments=appointments,stats=stats,graph=mock_graph())
+    
+    
+    url = f'{current_user['role']}/dashboard'
+    return redirect(url)
+
+
+@help_desk_bp.route('/appointments',methods=["POST","GET"])
 def help_desk_appointments():
     current_user = db.current_user()
     
