2d4c188df06c17af36ccec07bc22416ec2f012e7
Sabelo Mkhabela <transformmkhabela@gmail.com>
2026-01-23 13:25:34 +0200

    defined:save chats functionality

--- database/chat_database.py
+++ database/chat_database.py
@@ -1,51 +1,56 @@
 import os
 import json
 
+
 class ChatDb:
-    def save(self,doctor_id,question,response):
-        with open(os.path.join(os.getcwd(),'database','database_files','users.json'),'w') as file:
-            try:
-                data = self.read_data()
-                
-                try:
-                    for id,chats_list in data['doctor']['chats'].items():
-                    
-                        if id == doctor_id:
-                            chats_list.append(question)
-                            chats_list.append(response)
-                except:
-                    data = self.read_data()
-                    
-                    chats = data['doctor']['chats']
-                    chats.update({str(doctor_id):[question,response]})
-                    
-            except:
-                raise ValueError('Failed to open chats')
-                               
-                
+    def __init__(self):
+        os.makedirs('database/database_files/chats', exist_ok=True)
+        self.chats_path = os.path.join(
+            'database', 'database_files', 'chats', 'chats.json')
+
+        if not os.path.exists(self.chats_path):
+            with open(self.chats_path, 'x') as file:
+                file.write(json.dumps({"chats": {}}))
+                print('created chats.json')
+
+    def save(self, doctor_id, patient_id, question, response):
+        data = self.read_data()
+        data['chats'][doctor_id][patient_id].append((question,response))
+        print(data['chats'])
+        self.write_data(data)
+        # try:
+        #     data['chats'][doctor_id][patient_id].append(question, response)
+        #     self.write_data(data)
+        # except:
+        #     raise ValueError('Failed to save chats')
+
     def read_data(self):
-        with open(os.path.join(os.getcwd(),'database','database_files','users.json'),'r') as file:
+        with open(self.chats_path, 'r') as file:
             try:
                 data = json.loads(file.read())
-                
-                return data 
-            
+                return data
+
             except:
-                raise ValueError('Could not open file')
-            
-    def load_chats(self,id_doctor):
-        try:
-            data = self.read_data()
-            
+                raise ValueError('Could not open chats file at:')
+
+    def write_data(self, data):
+        with open(self.chats_path, 'w') as file:
             try:
-                for id,chats_list in data['doctor']['chats'].items():
-                    if id == id_doctor:
-                        return chats_list
-                    
+                file.write(json.dumps(data))
+
+                return True
+
             except:
-                return ('Failed to load chats')
-            
-                
-        except:
-            return ('failed to read chats database')
-        
+                raise ValueError('Could not save chats file')
+
+    def load_chats(self, doctor_id, patient_id):
+        data = self.read_data()
+
+        try:
+            chats = data['chats'][doctor_id][patient_id]
+            return chats
+
+        except KeyError:
+            data['chats'].update({doctor_id: {patient_id: []}})
+            self.write_data(data)
+            return []
