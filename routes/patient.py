018f372699e8b8c9266f11f1e31782aa09f29137
Sabelo Mkhabela <transformmkhabela@gmail.com>
2026-01-30 08:27:10 +0200

    defined route for updating the patient details

--- routes/patient.py
+++ routes/patient.py
@@ -8,3 +8,14 @@ db = Database()
 @patient_bp.route("/dashboard",methods=["GET"])
 def dashboard():
     return render_template('./patient/dashboard.html',user=db.current_user())
+
+@patient_bp.route("/update/profile",methods=["GET","POST"])
+def update_profile():
+    updated_info = request.get_json()
+    
+    result = db.update_patient_details(updated_info)
+    
+    if result:
+        return jsonify({'code':'200','msg':'Information updated'})
+    else:
+        return jsonify({'code':'500','msg':'Sorry we could not update your profile'})
